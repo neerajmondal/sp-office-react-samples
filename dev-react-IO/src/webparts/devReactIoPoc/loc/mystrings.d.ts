29b657b05ee4f9eca606adf956fd786d69b699b0
Neeraj K Mondal <neeraj.mondal@outlook.com>
2019-04-20 14:43:28 +0530

    First Commit to init Git Repo

---
+++ dev-react-IO/src/webparts/devReactIoPoc/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IDevReactIoPocWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'DevReactIoPocWebPartStrings' {
+  const strings: IDevReactIoPocWebPartStrings;
+  export = strings;
+}
